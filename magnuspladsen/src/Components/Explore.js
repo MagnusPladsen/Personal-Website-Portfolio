8f5df8b0a94e1badbc8cd47898ddfe99ec90442c
Magnus Pladsen <magnus_pladsen@hotmail.com>
2022-05-14 19:19:27 +0200

    Converting to React App

---
+++ magnuspladsen/src/Components/Explore.js
@@ -0,0 +1,25 @@
+import React from 'react';
+//import './Explore.css';
+
+
+class Explore extends React.Component {
+    render() {
+        return (
+            <div className="Explore">
+              <header className="Explore-header">
+                <div className="pb" alt="Profile picture"></div>
+                <h1>Magnus Pladsen</h1>
+                <h2>Junior Developer</h2>
+                <div className="icons">
+                  <a href="https://www.facebook.com/magnus.pladsen/" target="_blank"><div className="icon facebook"></div></a>
+                  <a href="https://www.linkedin.com/in/magnus-pladsen-1a2738226" target="_blank"><div className="icon linkedin"></div></a>
+                  <a href="https://github.com/MagnusPladsen" target="_blank"><div className="icon github"></div></a>
+                </div>
+                <button>Explore</button>
+              </header>
+            </div>
+        );
+    }
+}
+
+export default Explore;
